c0837c68880e7753d6dbed12af80f6594c7094a0
AmimoG <gilbertmatete6@gmail.com>
2020-08-20 16:36:14 +0300

    Add Files

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,2 +1,13 @@
-export class Highlight.Directive {
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  constructor(private elem: ElementRef) {
+    this.elem.nativeElement.style.color = 'gold';
+    this.elem.nativeElement.style.fontsize = '26px';
+   }
+
 }
